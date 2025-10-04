ecebd9d1d14b08b77dbcef5b2723e356503dc58f
sabidjanovv <sabidjanovv@gmail.com>
2025-10-04 17:52:58 +0500

    path fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
